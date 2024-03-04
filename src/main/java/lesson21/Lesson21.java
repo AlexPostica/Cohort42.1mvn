ff2b7dfd61010465218bd62420e700c60bb80fa2
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-04 18:23:00 +0100

    Create Lesson21.java

---
+++ src/main/java/lesson21/Lesson21.java
@@ -0,0 +1,12 @@
+package lesson21;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #21
+ * @author Alexandru Postica
+ * @version 1.Mar
+ */
+public class Lesson21 {
+    public static void main(String[] args) {
+
+    }
+}
