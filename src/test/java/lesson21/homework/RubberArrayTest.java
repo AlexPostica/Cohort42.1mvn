f6caa1c41d1f228ab759dc07f2c0db9c6a9ebab4
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-04 19:27:10 +0100

    Create RubberArrayTest.java

---
+++ src/test/java/lesson21/homework/RubberArrayTest.java
@@ -0,0 +1,14 @@
+package lesson21.homework;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class RubberArrayTest {
+
+    private RubberArray rubberArray;
+
+    @BeforeEach
+    public void init() {
+        rubberArray = new RubberArray();
+
+    }
+}
