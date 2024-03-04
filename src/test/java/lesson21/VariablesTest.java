f03e4ecb32d2008e50f72f7a21788190c3395e44
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-04 18:23:23 +0100

    Create VariablesTest.java

---
+++ src/test/java/lesson21/VariablesTest.java
@@ -0,0 +1,34 @@
+package lesson21;
+
+import extand.Variables;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VariablesTest {
+
+    @Test
+    public void testGetValue() {
+        Variables variables = new Variables();
+
+        // read empty valid variable
+        int a = variables.getValue("a");
+        Assertions.assertEquals(0, a);
+
+        // read variable with invalid name
+        Integer b = variables.getValue("ab");
+        Assertions.assertNull(b);
+
+        Integer c = variables.getValue("");
+        Assertions.assertNull(c);
+
+        Integer d = variables.getValue("A");
+        Assertions.assertNull(d);
+
+        // read not empty variable
+        variables.setValues("z", "123");
+        a = variables.getValue("z");
+        Assertions.assertEquals(123, a);
+
+
+    }
+}
